fcf2c53b721c0abe6d02659f984d5d1d49d534ce
6abotage <61562467+6abotage@users.noreply.github.com>
2022-07-11 23:52:49 +0200

    Add sample code from stackoverflow https://stackoverflow.com/questions/55763957/how-to-clean-up-setinterval-in-useeffect-using-react-hooks

--- src/App.js
+++ src/App.js
@@ -20,6 +20,23 @@ function transformTemplate({ rotate, translateX }) {
   return `rotate(${rotate}) translateX(${translateX})`;
 }
 
+function rotateThis(element, nR) {
+  var aR;
+  rot = rot || 0; // if rot undefined or 0, make 0, else rot
+  aR = rot % 360;
+  if (aR < 0) {
+    aR += 360;
+  }
+  if (aR < 180 && nR > aR + 180) {
+    rot -= 360;
+  }
+  if (aR >= 180 && nR <= aR - 180) {
+    rot += 360;
+  }
+  rot += nR - aR;
+  element.style.transform = "rotate( " + rot + "deg )";
+}
+
 export default function App() {
   const [degree, setDegree] = useState(0);
 
